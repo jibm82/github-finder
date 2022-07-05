e6a1e96c34380c4862c45587843cbbfabb9f1ff9
Jorge Baez <jibm_82@yahoo.com.mx>
2022-07-04 22:02:09 -0500

    Implement axios

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,19 +1,27 @@
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa"
 import { useEffect, useContext } from "react"
 import { Link, useParams } from "react-router-dom"
+
 import GithubContext from "../context/github/GithubContext"
+import { getUserAndRepos } from "../context/github/GithubActions"
+
 import Spinner from "../components/layout/Spinner"
 import RepoList from "../components/repos/RepoList"
 
 function User() {
-  const { getUser, user, isLoading, getUserRepos, repos } = useContext(GithubContext)
+  const { user, isLoading, repos, dispatch } = useContext(GithubContext)
   const params = useParams()
 
   useEffect(() => {
-    getUser(params.login)
-    getUserRepos(params.login)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    dispatch({ type: 'SET_LOADING' })
+
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login)
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData })
+    }
+
+    getUserData()
+  }, [dispatch, params.login])
 
   const {
     name,
